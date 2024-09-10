6d6513719ec397e506b73e1d9cca3e7030c1668c
isikbelkis <isikbelkis1@gmail.com>
2024-09-10 04:14:34 +0300

    Refactor and Update Search and Detail Fragments

---
+++ app/src/main/java/com/example/backgroundapp/view/BottomNavActivity.kt
@@ -0,0 +1,54 @@
+package com.example.backgroundapp.view
+
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.backgroundapp.R
+import com.example.backgroundapp.databinding.ActivityBattomNavBinding
+
+class BattomNavActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBattomNavBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityBattomNavBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        navView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> navController.navigate(R.id.homePageFragment)
+                R.id.nav_search -> navController.navigate(R.id.searchFragment)
+                R.id.nav_profile -> navController.navigate(R.id.profileFragment)
+            }
+            true
+        }
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.detailFragment -> hideBottomNav() // Belirli fragment'ta gizleme
+                else -> showBottomNav() // Diğer fragment'larda gösterme
+            }
+        }
+
+
+    }
+    private fun hideBottomNav() {
+        binding.navView.visibility = View.GONE
+    }
+
+    private fun showBottomNav() {
+        binding.navView.visibility = View.VISIBLE
+    }
+}
---
+++ app/src/main/java/com/example/backgroundapp/viewmodel/DetailViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.backgroundapp.viewmodel
+
+class DetailViewModel {
+}
